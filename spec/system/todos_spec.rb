3bdd144946731d01646093b92e8b78d8170a9b30
Chiara Nuñez <chiaraani@gmail.com>
2022-11-16 22:57:58 +0100

    System tests for creating, marking complete, marking incomplete and ordering todos

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -1,19 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Todos", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
-  scenario 'Creating a todo' do
+  scenario 'Creating todo' do
     visit root_path
-    byebug
 
-    fill_in 'todo', with: 'Do chores'
-
+    fill_in(placeholder: /Add/, with: 'Do chores')
     click_on 'Create Todo'
 
     expect(page).to have_content('Do chores')
     expect(page).to have_button('Mark complete')
+    expect(page).to have_link('Edit')
+    expect(page).to have_button('Delete')
+  end
+
+  scenario 'Marking todo complete' do
+    create :todo, status: :incomplete
+
+    visit root_path 
+    expect(find(':not(.complete).todo > .title')).to have_style('text-decoration' => /none/)
+
+    click_on 'Mark complete'
+
+    expect(find('.complete.todo > .title')).to have_style('text-decoration' => /line-through/)
+  end
+
+  scenario 'Marking todo incomplete' do
+    create :todo, status: :complete
+
+    visit root_path 
+    expect(find('.complete.todo > .title')).to have_style('text-decoration' => /line-through/)
+
+    click_on 'Mark incomplete'
+
+    expect(find(':not(.complete).todo > .title')).to have_style('text-decoration' => /none/)
+  end
+
+  scenario 'Ordering todos by status' do
+    todo1 = create :todo, title: 'Todo 1', status: :complete
+    todo2 = create :todo, title: 'Todo 2', status: :incomplete
+
+    visit root_path
+
+    expect(page).to have_text(/#{todo2.title}.+#{todo1.title}/m)
+
+    click_on 'Mark complete'
+
+    expect(page).to have_text(/#{todo1.title}.+#{todo2.title}/m)
   end
 end
